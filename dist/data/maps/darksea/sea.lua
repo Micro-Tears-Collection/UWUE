71ecbb2d54bc30c3e08cb89508e030eb721574f5
bigpeka <bigpeka@bigpeka-supreme>
2020-12-14 03:28:11 +0300

    Fix map

---
+++ dist/data/maps/darksea/sea.lua
@@ -0,0 +1,5 @@
+playSource("seagirl")
+
+setMusicPitch(0.2);
+
+showDialog("You say: Hello? Can you hear me?@...@ @She seems to explore a much deeper level than this.@...@@As you stare into everchanging pattern on her body, you start to lose yourself in it...@$cmd playSource(\"static_2\")@$cmd setMusicPitch(0.1)@And you just let it happen.@$cmd loadMap(\"blocks\",{fade={color=0x000000,time=9000}})")
