2632d177fa76174d9e6b1391c6306e0ee45e9aba
rmn20 <romax9lahin@gmail.com>
2022-02-15 22:06:35 +0300

    [default] секция в файлах материалов

--- src/code/engine3d/MaterialsFile.java
+++ src/code/engine3d/MaterialsFile.java
@@ -14,6 +14,7 @@ import java.util.Hashtable;
 public class MaterialsFile extends ReusableContent {
 	
 	private IniFile ini;
+	private Hashtable defaultVals;
 	
 	public MaterialsFile(String path) {
 		lock(); //never unload material files
@@ -22,6 +23,9 @@ public class MaterialsFile extends ReusableContent {
 		File f = new File("data", path);
 		if(f.exists()) {
 			ini = AssetManager.loadIni(path, true);
+			
+			int index = search(ini.keys(), "default");
+			if(index >= 0) defaultVals = ini.hashtables()[index];
 		}
 	}
 	
@@ -29,32 +33,45 @@ public class MaterialsFile extends ReusableContent {
 		String matName = matPath.substring(matPath.lastIndexOf('/') + 1);
 		
 		if(ini != null && ini.groupExists(matName)) {
-			Hashtable[] groups = ini.hashtables();
-			String[] groupNames = ini.keys();
+			int index = search(ini.keys(), matName);
+			Hashtable matTable = ini.hashtables()[index];
 
-			for(int i=0; i<groups.length; i++) {
-				
-				if(groupNames[i].equals(matName)) {
-					Hashtable matTable = groups[i];
+			Enumeration optionNames = matTable.keys();
+			Enumeration options = matTable.elements();
 
-					Enumeration optionNames = matTable.keys();
-					Enumeration options = matTable.elements();
+			for(int x=0; x<matTable.size(); x++) {
+				String optionName = (String) optionNames.nextElement();
+				String option = (String) options.nextElement();
 
-					for(int x=0; x<matTable.size(); x++) {
-						String optionName = (String) optionNames.nextElement();
-						String option = (String) options.nextElement();
+				if(matIni.get(optionName) == null) {
+					matIni.put(optionName, option);
+				}
+			}
+		}
+		
+		if(defaultVals != null) {
+			Enumeration optionNames = defaultVals.keys();
+			Enumeration options = defaultVals.elements();
 
-						if(matIni.get(optionName) == null) {
-							matIni.put(optionName, option);
-						}
-					}
-					
-					break;
+			for(int x=0; x<defaultVals.size(); x++) {
+				String optionName = (String) optionNames.nextElement();
+				String option = (String) options.nextElement();
+
+				if(matIni.get(optionName) == null) {
+					matIni.put(optionName, option);
 				}
 			}
 		}
 		
 		if(matIni.get("albedo") == null) matIni.put("albedo", matPath + ".png");
+	}
+	
+	private int search(String[] keys, String key) {
+		for(int i=0; i<keys.length; i++) {
+			if(keys[i].equals(key)) return i;
+		}
+		
+		return -1;
 	}
 
 }
