6fdd1115b5101681ff711b3aa62c48dfd14eb069
rmn20 <romax9lahin@gmail.com>
2022-09-04 23:28:03 +0300

    Переношу игровые ресурсы в другой репозиторий

--- dist/data/maps/darksea/sea.lua
+++
@@ -1,5 +0,0 @@
-playSource("seagirl")
-
-setMusicPitch(0.2);
-
-showDialog("You:*Hello? Can you hear me?@...@ @She seems to explore a much deeper level than this.@...@@As you stare into everchanging pattern on her body, you start to lose yourself in it...@$cmd playSource(\"static_2\")@$cmd setMusicPitch(0.1)@And you just let it happen.@$cmd loadMap(\"pike\",{fade={color=0x000000,time=9000}})")
--- dist/data/maps/grassy/caretaker_door.lua
+++
@@ -1,9 +0,0 @@
-if objVar("player", "x") < -4175 then 
-objVar("player", "pos", {-4115, -270, -5535})
-objVar("mistress", "pos", {-3231, -270, -6615})
-objVar("mistress", "rotY", 0)
-setMusicPitch(0.7) 
-else
-objVar("player", "pos", {-4245, -270, -5535})
-setMusicPitch(1)
-end
